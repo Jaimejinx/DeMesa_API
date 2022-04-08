cc05651dad2615a85537ec4c2355b3ae56316a52
Christian Jaime De Mesa <cm.demesa@mseuf.edu.ph>
2022-04-08 16:46:58 +0800

    Modified all the errors

--- myAPI/urls.py
+++ myAPI/urls.py
@@ -1,5 +1,4 @@
-from xml.etree.ElementInclude import include
-from djnago.urls import inlcude, path
+from django.urls import include , path
 from rest_framework import routers
 from . import views
 
@@ -9,5 +8,5 @@ router.register('vehicls', views.vehiclsviewsets)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- myAPI/views.py
+++ myAPI/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_frameworks import viewsets
+from rest_framework import viewsets
 from . serializers import vehiclsSerializer
 from . models import vehicls
 
 class vehiclsviewsets(viewsets.ModelViewSet):
     queryset = vehicls.objects.all().order_by('name')
-    serializers_class = vehiclsSerializer
+    serializer_class = vehiclsSerializer
 
 # Create your views here.
