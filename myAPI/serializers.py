d29822797cc01f96dcda2ecd061aaf672bced22e
Christian Jaime De Mesa <cm.demesa@mseuf.edu.ph>
2022-04-08 16:14:26 +0800

    Modified views.py and urls.py

--- myAPI/serializers.py
+++ myAPI/serializers.py
@@ -2,7 +2,7 @@ from dataclasses import fields
 from rest_framework import serializers
 from . models import vehicls
 
-class vehiclsSerializers(serializers.HyperlinkedModelSerializer):
+class vehiclsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = vehicls
         fields = ('name', 'description')
